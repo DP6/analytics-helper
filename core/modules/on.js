2c5710c596f2e2284d2f4b4884a264ea9a22774c
TrivilinVitor <32498509+TrivilinVitor@users.noreply.github.com>
2017-10-11 15:24:41 -0300

    Update on.js
    adição da função delegate

--- core/modules/on.js
+++ core/modules/on.js
@@ -37,4 +37,42 @@ function on (event, selector, callback) {
     }
   }
 }
+
+/**
+ *Executa um callback ao executar algum evento 
+ *em um elemento HTML específico que possa estar 
+ *inicialmente oculto ou se ocultar em uma página responsiva.
+ * @param {*} event Evento que ira executar o callback, exemplos: 
+ *'mousedown', 'click'.
+ * @param {*} selector Seletor CSS que irá buscar os elementos 
+ *que executarão o callback no disparo do evento.
+ * @param {*} handler Função executada no disparo do evento.
+ * @param {*} parent elemento presente inicialmente na página 
+ *e que não é ocultado. Defaut é o document da página.
+ */
+function delegate (event, selector, handler, parent) {
+  if (typeof jQuery === "function") {
+    var elem = jQuery(parent || document);
+    if (typeof elem.on === "function") {
+      return elem.on(event, selector, handler);
+    } else if (typeof elem.delegate === "function") {
+      return elem.delegate(selector, event, handler);
+    }
+  }
+  if (typeof document.addEventListener === "function") {
+    var method = "addEventListener";
+  } else {
+    var method = "attachEvent";
+    event = "on" + event;
+  }
+  (parent || document)[method](event, function (e) {
+    for (var target = e.target; target && target != this; target = target.parentNode) {
+      // loop parent nodes from the target to the delegation node
+      if (match(target, selector)) {
+        handler.call(target, e);
+        break;
+      }
+    }
+  }, false);
+}
 helper.on = on;
