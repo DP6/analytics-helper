b293c4be67dea7f66a9a068a9d5d9b5b23a7f299
Joaquim Neto <joaquim@localhost.localdomain>
2019-05-27 16:39:18 -0300

    perf(*): implementaçao estável do travis, semantic-release e TDD

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,42 @@
 'use strict';
 
-const gulp = require('gulp');
-const concat = require('gulp-concat');
-const beautify = require('gulp-beautify');
-const include = require('gulp-include');
-const strip = require('gulp-strip-comments');
-const del = require('del');
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var beautify = require('gulp-beautify');
+var include = require('gulp-include');
+var strip = require('gulp-strip-comments');
+var del = require('del');
+var replace = require('gulp-replace');
 
-gulp.task('gtm-modules', () =>
-  gulp
-  .src(['./core/modules/*.js', './gtm/modules/*js'])
-  .pipe(concat('gtm-modules.js'))
-  .pipe(beautify({
-    indent_size: 2
-  }))
-  .pipe(strip())
-  .on('error', console.log)
-  .pipe(gulp.dest('./tmp'))
-);
+gulp.task('gtm-modules', function() {
+  return gulp.src(['./core/modules/*.js', './gtm/modules/*js'])
+    .pipe(replace(/module.exports[a-z-A-Z.]*\s*=\s*([a-zA-Z\-_]+;|([a-zA-Z\-_:.={},\n\s]+);+)/g, ''))
+    .pipe(concat('gtm-modules.js'))
+    .pipe(beautify({
+        indent_size: 2,
+        max_preserve_newlines: 2
+    }))
+    .pipe(strip())
+    .on('error', console.log)
+    .pipe(gulp.dest('./tmp'));
+});
 
-gulp.task('build-gtm', () =>
-  gulp
-  .src('./gtm/main.js')
-  .pipe(include({
-    includePaths: ['/tmp', '/gtm'].map(path => __dirname + path),
-    hardFail: true
-  }))
-  .on('error', console.log)
-  .pipe(gulp.dest('./build/gtm'))
-);
+gulp.task('build-gtm', function() {
+  return gulp.src('./gtm/main.js')
+    .pipe(replace(/module.exports\s*=\s*[a-zA-Z]+;/g, ''))
+    .pipe(include({
+      hardFail: true,
+      includePaths: [
+        __dirname + '/tmp',
+        __dirname + '/gtm',
+      ]
+    }))
+    .on('error', console.log)
+    .pipe(gulp.dest('./build/gtm'));
+});
 
-gulp.task('clean', () => del(['./tmp']));
+gulp.task('clean', function() {
+  return del(['./tmp']);
+});
 
 gulp.task('default', gulp.series(['clean', 'gtm-modules', 'build-gtm', 'clean']));
--- test/index.test.js
+++ test/index.test.js
@@ -2,3 +2,5 @@
 
 require('./core/modules/sanitize.test.js');
 require('./core/modules/has.test.js');
+require('./core/modules/text.test.js');
+require('./core/modules/cookie.test.js');
