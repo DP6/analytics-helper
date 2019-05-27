3465808eaaae5a6b4a62d1fd6c9d822fc3d5fb6d
Joaquim Neto <joaquim.neto@dp6.com.br>
2019-05-27 16:39:18 -0300

    perf(*): implementaçao estável do travis, semantic-release e testes unitários

--- test/core/modules/has.test.js
+++ test/core/modules/has.test.js
@@ -4,15 +4,15 @@ const expect = require('chai').expect;
 const has = require('../../.././core/modules/has.js');
 
 describe('Core', () => {
-    describe('Modules', () => {
-        describe('.has(obj, key)', () => {
-            it('should export a function', () => {
-                expect(has).to.be.a('function');
-            });
-
-            it('should return true if object has attribute with name of the key', () => {
-                expect(has({ name: 'foo' }, 'name')).true;
-            });
-        });
+  describe('Modules', () => {
+    describe('.has(obj, key)', () => {
+      it('should export a function', () => {
+        expect(has).to.be.a('function');
+      });
+      
+      it('should return true if object has attribute with name of the key', () => {
+        expect(has({ name: 'foo' }, 'name')).true;
+      });
     });
+  });
 });
