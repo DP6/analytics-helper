0f59b296d53b7719cd2742d1ad22c031fd02f1c2
Joaquim Neto <joaquim.neto@dp6.com.br>
2019-05-30 17:58:37 -0300

    fix(closest.js): remoção da marção dos merge não realizado

--- build/gtm/main.js
+++ build/gtm/main.js
@@ -54,18 +54,9 @@
         return parent;
       }
       parent = parent.parentNode;
-    } <<
-    << << < HEAD
+    }
     return undefined;
   }
-  
-  ===
-  === =
-  parent = parent.parentNode;
-  }
-  return undefined;
-  } >>>
-  >>> > 60857 a0af0a8d66b3b33ee32cf24b88330255241
   
   function getCookie(key) {
     key = '; ' + key + '=';
